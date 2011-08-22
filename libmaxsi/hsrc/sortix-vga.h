7510708ea15c830d5c107d953f057bad371cbafc
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2011-08-22 11:11:04 +0200

    Gave user-space access to VGA color constants.

--- libmaxsi/hsrc/sortix-vga.h
+++ libmaxsi/hsrc/sortix-vga.h
@@ -30,6 +30,24 @@ namespace System
 {
 	namespace VGA
 	{
+		// TODO: Move these to a better place
+		#define COLOR8_BLACK 0
+		#define COLOR8_BLUE 1
+		#define COLOR8_GREEN 2
+		#define COLOR8_CYAN 3
+		#define COLOR8_RED 4
+		#define COLOR8_MAGENTA 5
+		#define COLOR8_BROWN 6
+		#define COLOR8_LIGHT_GREY 7
+		#define COLOR8_DARK_GREY 8
+		#define COLOR8_LIGHT_BLUE 9
+		#define COLOR8_LIGHT_GREEN 10
+		#define COLOR8_LIGHT_CYAN 11
+		#define COLOR8_LIGHT_RED 12
+		#define COLOR8_LIGHT_MAGENTA 13
+		#define COLOR8_LIGHT_BROWN 14
+		#define COLOR8_WHITE 15
+
 		// This is the contents of a VGA framebuffer used in text mode. The
 		// lower 8 lower bits correspond to an ASCII character, the next 4 bits
 		// is the text color, and the upper 4 bits are the background color.
