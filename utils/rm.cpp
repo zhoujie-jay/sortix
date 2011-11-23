481ddfa7e4ae85cd5ddc11d09932b27f3e9413cc
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2011-11-23 17:05:09 +0100

    rm(1) usage now works without crashing.

--- utils/rm.cpp
+++ utils/rm.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char* argv[])
 {
-	if ( argc < 2 ) { printf("usage: %s <file> ...\n"); return 0; }
+	if ( argc < 2 ) { printf("usage: %s <file> ...\n", argv[0]); return 0; }
 
 	int result = 0;
 
