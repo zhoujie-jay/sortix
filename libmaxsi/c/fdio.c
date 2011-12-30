73a7e8f53d07da982ca1e90c25b2cdbf502a4c10
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2011-12-30 01:13:41 +0100

    Forgot to actually add remove(3) in the previous commit.

--- libmaxsi/c/fdio.c
+++ libmaxsi/c/fdio.c
@@ -191,3 +191,14 @@ FILE* fopen(const char* path, const char* mode)
 	return fp;
 }
 
+int remove(const char* pathname)
+{
+	int result = unlink(pathname);
+	if ( result && errno == EISDIR )
+	{
+		// TODO: rmdir is unimplemented.
+		// result = rmdir(pathname);
+	}
+	return result;
+}
+
