a7658d5b8ccdc25c2d4291573ca2a5840112519d
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2012-03-07 18:06:23 +0100

    uname(1) now uses kernelinfo(2) to determine kernel version.

--- utils/uname.cpp
+++ utils/uname.cpp
@@ -1,7 +1,18 @@
+#include <sys/kernelinfo.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+#include <error.h>
 
 int main(int argc, char* argv[])
 {
-	printf("Sortix %zu bit\n", sizeof(size_t) * 8UL);
+	size_t VERSIONSIZE = 256UL;
+	char version[VERSIONSIZE];
+	if ( kernelinfo("version", version, VERSIONSIZE) )
+	{
+		error(1, errno, "kernelinfo(\"version\")");
+	}
+	printf("Sortix %zu bit (%s)\n", sizeof(size_t) * 8UL, version);
 	return 0;
 }
+
