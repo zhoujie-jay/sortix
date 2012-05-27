500f8651bb2cc19e26af24f9070341e82c7de630
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2012-05-27 23:46:59 +0200

    Prevent GCC from fixing headers that are OK.

--- libmaxsi/decl/size_t.h
+++ libmaxsi/decl/size_t.h
@@ -2,6 +2,9 @@
 #ifndef _SIZE_T_DECLARED /* Compabillity with gcc */
 #define _SIZE_T_DECL
 #define _SIZE_T_DECLARED
+#if !defined(_GCC_SIZE_T)
+#define _GCC_SIZE_T
 typedef __size_t size_t;
+#endif
 #endif
 #endif
--- libmaxsi/decl/wchar_t.h
+++ libmaxsi/decl/wchar_t.h
@@ -1,6 +1,9 @@
 #ifndef _WCHAR_T_DECL
 #define _WCHAR_T_DECL
 #if !defined(__cplusplus)
+#if !defined(_GCC_WCHAR_T)
+#define _GCC_WCHAR_T
 typedef __wchar_t wchar_t;
+#endif
 #endif
 #endif
