271c64e5376a0df82ba729ccf7d5554296e8213b
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2012-05-28 22:57:03 +0200

    Added a stub locale.h header.

---
+++ libmaxsi/include/locale.h
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+	COPYRIGHT(C) JONAS 'SORTIE' TERMANSEN 2012.
+
+	This file is part of LibMaxsi.
+
+	LibMaxsi is free software: you can redistribute it and/or modify it under
+	the terms of the GNU Lesser General Public License as published by the Free
+	Software Foundation, either version 3 of the License, or (at your option)
+	any later version.
+
+	LibMaxsi is distributed in the hope that it will be useful, but WITHOUT ANY
+	WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+	FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for
+	more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with LibMaxsi. If not, see <http://www.gnu.org/licenses/>.
+
+	locale.h
+	Category macros.
+
+*******************************************************************************/
+
+#ifndef _LOCALE_H
+#define _LOCALE_H 1
+
+#include <features.h>
+
+__BEGIN_DECLS
+
+/* TODO: Actually implement this header. */
+
+__END_DECLS
+
+#endif
+
