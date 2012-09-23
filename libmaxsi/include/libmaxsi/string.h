b732c46bf50411b0e93a7e46249cca3760e71b57
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2012-09-23 12:33:34 +0200

    Remove unused declarations from <libmaxsi/string.h>.

--- libmaxsi/include/libmaxsi/string.h
+++ libmaxsi/include/libmaxsi/string.h
@@ -29,29 +29,10 @@ namespace Maxsi
 {
 	namespace String
 	{
-		// TODO: Remove this once. Use \e instead.
-		const char ASCII_ESCAPE = 27;
-
-		size_t Length(const char* String);
-		size_t Accept(const char* str, const char* accept);
-		size_t Reject(const char* str, const char* reject);
 		char* Clone(const char* Source);
 		char* Combine(size_t NumParameters, ...);
 		char* Substring(const char* src, size_t offset, size_t length);
-		char* Copy(char* Dest, const char* Src);
-		char* Cat(char* Dest, const char* Src);
-		char* Tokenize(char* str, const char* delim);
-		char* TokenizeR(char* str, const char* delim, char** saveptr);
-		char* Seek(const char* str, int c);
-		char* SeekNul(const char* str, int c);
-		char* SeekReverse(const char* str, int c);
-		int Compare(const char* A, const char* B);
-		int CompareN(const char* A, const char* B, size_t MaxLength);
 		bool StartsWith(const char* Haystack, const char* Needle);
-		int ToInt(const char* str);
-
-		int ConvertUInt32(uint32_t num, char* dest);
-		int ConvertUInt64(uint64_t num, char* dest);
 	}
 }
 
