6b663d04d2e873178433ec7f4a117b0517820066
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2012-09-29 00:36:46 +0200

    Fix libc copyright notices.

--- libmaxsi/SIG_ERR.cpp
+++ libmaxsi/SIG_ERR.cpp
@@ -2,20 +2,20 @@
 
 	Copyright(C) Jonas 'Sortie' Termansen 2011, 2012.
 
-	This file is part of LibMaxsi.
+	This file is part of the Sortix C Library.
 
-	LibMaxsi is free software: you can redistribute it and/or modify it under
-	the terms of the GNU Lesser General Public License as published by the Free
-	Software Foundation, either version 3 of the License, or (at your option)
-	any later version.
+	The Sortix C Library is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or (at your
+	option) any later version.
 
-	LibMaxsi is distributed in the hope that it will be useful, but WITHOUT ANY
-	WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-	FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
-	details.
+	The Sortix C Library is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+	or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+	License for more details.
 
 	You should have received a copy of the GNU Lesser General Public License
-	along with LibMaxsi. If not, see <http://www.gnu.org/licenses/>.
+	along with the Sortix C Library. If not, see <http://www.gnu.org/licenses/>.
 
 	SIG_ERR.cpp
 	Abort on signal.
