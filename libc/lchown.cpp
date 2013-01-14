deeedf9e5d90b61543bd7efc99f917746ebb3f66
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2013-01-14 22:58:34 +0100

    Add lchown(2).

---
+++ libc/lchown.cpp
@@ -0,0 +1,33 @@
+/*******************************************************************************
+
+    Copyright(C) Jonas 'Sortie' Termansen 2013.
+
+    This file is part of the Sortix C Library.
+
+    The Sortix C Library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    The Sortix C Library is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with the Sortix C Library. If not, see <http://www.gnu.org/licenses/>.
+
+    lchown.cpp
+    Changes the owner and group of a file.
+
+*******************************************************************************/
+
+#include <sys/types.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+
+extern "C" int lchown(const char* path, uid_t owner, gid_t group)
+{
+	return fchownat(AT_FDCWD, path, owner, group, AT_SYMLINK_NOFOLLOW);
+}
