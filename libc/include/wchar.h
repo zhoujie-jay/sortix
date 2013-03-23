3ec03badf3ec71dd545a8c4dbd3c1c8bb01b3d02
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2013-03-24 00:43:59 +0100

    Add wcsncat(3).

--- libc/include/wchar.h
+++ libc/include/wchar.h
@@ -69,6 +69,7 @@ wchar_t* wcschrnul(const wchar_t*, wchar_t);
 int wcscmp(const wchar_t*, const wchar_t*);
 wchar_t* wcscpy(wchar_t* restrict, const wchar_t* restrict);
 size_t wcslen(const wchar_t*);
+wchar_t* wcsncat(wchar_t* restrict, const wchar_t* restrict, size_t);
 
 /* TODO: These are not implemented in sortix libc yet. */
 #if defined(__SORTIX_SHOW_UNIMPLEMENTED)
@@ -109,7 +110,6 @@ size_t wcsxfrm(wchar_t* restrict, const wchar_t* restrict, size_t);
 unsigned long long wcstoull(const wchar_t* restrict, wchar_t** restrict, int);
 unsigned long wcstoul(const wchar_t* restrict, wchar_t** restrict, int);
 wchar_t* fgetws(wchar_t* restrict, int, FILE* restrict);
-wchar_t* wcsncat(wchar_t* restrict, const wchar_t* restrict, size_t);
 wchar_t* wcsncpy(wchar_t* restrict, const wchar_t* restrict, size_t);
 wchar_t* wcspbrk(const wchar_t*, const wchar_t*);
 wchar_t* wcsrchr(const wchar_t*, wchar_t);
