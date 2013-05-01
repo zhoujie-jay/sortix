b3e7aafff8dd3260b8e7a429d9bb6dc3f91a1850
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2013-05-01 22:01:09 +0200

    Add psignal(3).

---
+++ libc/psignal.cpp
@@ -0,0 +1,34 @@
+/*******************************************************************************
+
+    Copyright(C) Jonas 'Sortie' Termansen 2013.
+
+    This file is part of the Sortix C Library.
+
+    The Sortix C Library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    The Sortix C Library is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with the Sortix C Library. If not, see <http://www.gnu.org/licenses/>.
+
+    psignal.cpp
+    Print signal error condition to stderr.
+
+*******************************************************************************/
+
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+
+extern "C" void psignal(int signum, const char* message)
+{
+	if ( message && message[0] )
+		fprintf(stderr, "%s: ", message);
+	fprintf(stderr, strsignal(signum));
+}
