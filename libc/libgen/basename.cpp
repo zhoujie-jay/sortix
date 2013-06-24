9c4f25ae7c120fd507c5c30b4191b8e56a86230f
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2013-06-24 23:07:11 +0200

    Move libgen.h functions into their own directory.

--- libc/libgen/basename.cpp
+++ libc/libgen/basename.cpp
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with the Sortix C Library. If not, see <http://www.gnu.org/licenses/>.
 
-    basename.cpp
+    libgen/basename.cpp
     Returns the name part of a path.
 
 *******************************************************************************/
--- libc/libgen/dirname.cpp
+++ libc/libgen/dirname.cpp
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with the Sortix C Library. If not, see <http://www.gnu.org/licenses/>.
 
-    dirname.cpp
+    libgen/dirname.cpp
     Returns the directory part of a path.
 
 *******************************************************************************/
