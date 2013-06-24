50ae094c6f2c641d1ce82d39604ab7255be0ed25
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2013-06-25 00:22:24 +0200

    Move poll.h functions into their own directory.

--- libc/poll/poll.cpp
+++ libc/poll/poll.cpp
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with the Sortix C Library. If not, see <http://www.gnu.org/licenses/>.
 
-    poll.cpp
+    poll/poll.cpp
     Input/output multiplexing.
 
 *******************************************************************************/
--- libc/poll/ppoll.cpp
+++ libc/poll/ppoll.cpp
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with the Sortix C Library. If not, see <http://www.gnu.org/licenses/>.
 
-    ppoll.cpp
+    poll/ppoll.cpp
     Input/output multiplexing.
 
 *******************************************************************************/
