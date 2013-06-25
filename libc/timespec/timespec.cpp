a329235397ec3903a843b0b4adb0ee933008e828
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2013-06-25 15:27:48 +0200

    Move timespec.h functions into their own directory.

--- libc/timespec/timespec.cpp
+++ libc/timespec/timespec.cpp
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with the Sortix C Library. If not, see <http://www.gnu.org/licenses/>.
 
-    timespec.cpp
+    timespec/timespec.cpp
     Utility functions for manipulation of struct timespec.
 
 *******************************************************************************/
