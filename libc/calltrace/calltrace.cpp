03ac25a6e2a225960b80cd1e7d5add51037d960a
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2013-06-25 12:50:37 +0200

    Move calltrace.h functions into their own directory.

--- libc/calltrace/calltrace.cpp
+++ libc/calltrace/calltrace.cpp
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with the Sortix C Library. If not, see <http://www.gnu.org/licenses/>.
 
-    calltrace.cpp
+    calltrace/calltrace.cpp
     Traverses the stack and prints the callstack.
 
 *******************************************************************************/
