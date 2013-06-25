e8bc31b7985d9cd4e88fcc5a9fcd978a463c9a0f
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2013-06-25 13:01:37 +0200

    Move sys/select.h functions into their own directory.

--- libc/sys/select/select.cpp
+++ libc/sys/select/select.cpp
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with the Sortix C Library. If not, see <http://www.gnu.org/licenses/>.
 
-    select.cpp
+    sys/select/select.cpp
     Waiting on multiple file descriptors.
 
 *******************************************************************************/
