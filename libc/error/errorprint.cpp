575e53644291698e43fac9096c181c35519e5f60
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2013-06-25 13:08:01 +0200

    Move error.h functions into their own directory.

--- libc/error/errorprint.cpp
+++ libc/error/errorprint.cpp
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with the Sortix C Library. If not, see <http://www.gnu.org/licenses/>.
 
-    errorprint.cpp
+    error/errorprint.cpp
     Functions for printing error messages to the terminal.
 
 *******************************************************************************/
