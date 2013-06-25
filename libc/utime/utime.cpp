c4715aef59386ddc0fbc87b29c2980c7c4cb57c7
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2013-06-25 12:52:58 +0200

    Move utime.h functions into their own directory.

--- libc/utime/utime.cpp
+++ libc/utime/utime.cpp
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with the Sortix C Library. If not, see <http://www.gnu.org/licenses/>.
 
-    utime.cpp
+    utime/utime.cpp
     Change file last access and modification times.
 
 *******************************************************************************/
