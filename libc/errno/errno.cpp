bd15083e6583e188ef62009348a38daebeaaf6b8
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2013-06-25 13:03:58 +0200

    Move errno.h functions into their own directory.

--- libc/errno/errno.cpp
+++ libc/errno/errno.cpp
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with the Sortix C Library. If not, see <http://www.gnu.org/licenses/>.
 
-    errno.cpp
+    errno/errno.cpp
     Value storing a numeric value representing the last occured error.
 
 *******************************************************************************/
