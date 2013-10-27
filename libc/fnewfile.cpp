c109f5165eb71134402057d8b15f13a3d81f4fa5
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2013-10-27 22:30:56 +0100

    Fix fnewfile(3) being in a file with the wrong name.

--- libc/fnewfile.cpp
+++ libc/fnewfile.cpp
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with the Sortix C Library. If not, see <http://www.gnu.org/licenses/>.
 
-    fnewline.cpp
+    fnewfile.cpp
     Allocates and initializes a simple FILE object ready for construction.
 
 *******************************************************************************/
