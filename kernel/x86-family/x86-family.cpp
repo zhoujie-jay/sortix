6d79781497baf05cc98f9c2260076892a35f7fe8
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2014-01-04 02:12:09 +0100

    Update kernel/x86-family/x86-family.{cpp,h} to current coding conventions.

--- kernel/x86-family/x86-family.cpp
+++ kernel/x86-family/x86-family.cpp
@@ -22,87 +22,90 @@
 
 *******************************************************************************/
 
+#include <stdint.h>
+
 #include <sortix/kernel/kernel.h>
 
-namespace Sortix
+namespace Sortix {
+namespace CPU {
+
+void OutPortB(uint16_t port, uint8_t value)
 {
-	namespace CPU
-	{
-		void OutPortB(uint16_t Port, uint8_t Value)
-		{
-			asm volatile ("outb %1, %0" : : "dN" (Port), "a" (Value));
-		}
-
-		void OutPortW(uint16_t Port, uint16_t Value)
-		{
-			asm volatile ("outw %1, %0" : : "dN" (Port), "a" (Value));
-		}
-
-		void OutPortL(uint16_t Port, uint32_t Value)
-		{
-			asm volatile ("outl %1, %0" : : "dN" (Port), "a" (Value));
-		}
-
-		uint8_t InPortB(uint16_t Port)
-		{
-			uint8_t Result;
-			asm volatile("inb %1, %0" : "=a" (Result) : "dN" (Port));
-			return Result;
-		}
-
-		uint16_t InPortW(uint16_t Port)
-		{
-			uint16_t Result;
-			asm volatile("inw %1, %0" : "=a" (Result) : "dN" (Port));
-			return Result;
-		}
-
-		uint32_t InPortL(uint16_t Port)
-		{
-			uint32_t Result;
-			asm volatile("inl %1, %0" : "=a" (Result) : "dN" (Port));
-			return Result;
-		}
-
-		void Reboot()
-		{
-			// Keyboard interface IO port: data and control.
-			const uint16_t KEYBOARD_INTERFACE = 0x64;
-
-			// Keyboard IO port.
-			const uint16_t KEYBOARD_IO = 0x60;
-
-			// Keyboard data is in buffer (output buffer is empty) (bit 0).
-			const uint8_t KEYBOARD_DATA = (1<<0);
-
-			// User data is in buffer (command buffer is empty) (bit 1).
-			const uint8_t USER_DATA = (1<<1);
-
-			// Disable interrupts.
-			asm volatile("cli");
-
-			// Clear all keyboard buffers (output and command buffers).
-			uint8_t byte;
-			do
-			{
-				byte = InPortB(KEYBOARD_INTERFACE);
-				if ( ( byte & KEYBOARD_DATA ) != 0 ) { InPortB(KEYBOARD_IO); }
-			} while ( ( byte & USER_DATA ) != 0 );
-
-			// CPU reset command.
-			uint8_t KEYBOARD_RESET_CPU = 0xFE;
-
-			// Now pulse the CPU reset line and reset.
-			OutPortB(KEYBOARD_INTERFACE, KEYBOARD_RESET_CPU);
-
-			// If that didn't work, just halt.
-			asm volatile("hlt");
-		}
-
-		void ShutDown()
-		{
-			// TODO: Unimplemented, just reboot.
-			Reboot();
-		}
-	}
+	asm volatile ("outb %1, %0" : : "dN" (port), "a" (value));
 }
+
+void OutPortW(uint16_t port, uint16_t value)
+{
+	asm volatile ("outw %1, %0" : : "dN" (port), "a" (value));
+}
+
+void OutPortL(uint16_t port, uint32_t value)
+{
+	asm volatile ("outl %1, %0" : : "dN" (port), "a" (value));
+}
+
+uint8_t InPortB(uint16_t port)
+{
+	uint8_t result;
+	asm volatile("inb %1, %0" : "=a" (result) : "dN" (port));
+	return result;
+}
+
+uint16_t InPortW(uint16_t port)
+{
+	uint16_t result;
+	asm volatile("inw %1, %0" : "=a" (result) : "dN" (port));
+	return result;
+}
+
+uint32_t InPortL(uint16_t port)
+{
+	uint32_t result;
+	asm volatile("inl %1, %0" : "=a" (result) : "dN" (port));
+	return result;
+}
+
+void Reboot()
+{
+	// Keyboard interface IO port: data and control.
+	const uint16_t KEYBOARD_INTERFACE = 0x64;
+
+	// Keyboard IO port.
+	const uint16_t KEYBOARD_IO = 0x60;
+
+	// Keyboard data is in buffer (output buffer is empty) (bit 0).
+	const uint8_t KEYBOARD_DATA = 1 << 0;
+
+	// User data is in buffer (command buffer is empty) (bit 1).
+	const uint8_t USER_DATA = 1 << 1;
+
+	// Disable interrupts.
+	asm volatile("cli");
+
+	// Clear all keyboard buffers (output and command buffers).
+	uint8_t byte;
+	do
+	{
+		byte = InPortB(KEYBOARD_INTERFACE);
+		if ( byte & KEYBOARD_DATA )
+			InPortB(KEYBOARD_IO);
+	} while ( byte & USER_DATA );
+
+	// CPU reset command.
+	uint8_t KEYBOARD_RESET_CPU = 0xFE;
+
+	// Now pulse the CPU reset line and reset.
+	OutPortB(KEYBOARD_INTERFACE, KEYBOARD_RESET_CPU);
+
+	// If that didn't work, just halt.
+	asm volatile("hlt");
+}
+
+void ShutDown()
+{
+	// TODO: Unimplemented, just reboot.
+	Reboot();
+}
+
+} // namespace CPU
+} // namespace Sortix
