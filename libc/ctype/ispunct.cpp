ba8557075c0f32d1a9fcd10ad585ca864ca2d7de
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2014-01-07 21:36:01 +0100

    Split libc/ctype/ctype.cpp into multiple files.

---
+++ libc/ctype/ispunct.cpp
@@ -0,0 +1,30 @@
+/*******************************************************************************
+
+    Copyright(C) Jonas 'Sortie' Termansen 2011.
+
+    This file is part of the Sortix C Library.
+
+    The Sortix C Library is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    The Sortix C Library is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+    or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with the Sortix C Library. If not, see <http://www.gnu.org/licenses/>.
+
+    ctype/ispunct.cpp
+    Returns whether the character is punctuational.
+
+*******************************************************************************/
+
+#include <ctype.h>
+
+extern "C" int ispunct(int c)
+{
+	return isprint(c) && c != ' ' && !isalnum(c);
+}
