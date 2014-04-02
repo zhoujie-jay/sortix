7f7fd6603959fe25da2ce86d78be6ab60e2e91b6
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2014-04-02 17:56:48 +0200

    Add isascii(3).

--- libc/include/ctype.h
+++ libc/include/ctype.h
@@ -1,6 +1,6 @@
 /*******************************************************************************
 
-    Copyright(C) Jonas 'Sortie' Termansen 2011.
+    Copyright(C) Jonas 'Sortie' Termansen 2011, 2014.
 
     This file is part of the Sortix C Library.
 
@@ -22,30 +22,51 @@
 
 *******************************************************************************/
 
-/* TODO: POSIX-1.2008 compliance is only partial */
-
-#ifndef _CTYPE_H
-#define _CTYPE_H 1
+#ifndef INCLUDE_CTYPE_H
+#define INCLUDE_CTYPE_H
 
 #include <sys/cdefs.h>
 
 __BEGIN_DECLS
 
-/* TODO: Declare locale_t and the *_t functions here */
+#ifndef __locale_t_defined
+#define __locale_t_defined
+/* TODO: figure out what this does and typedef it properly. This is just a
+         temporary assignment. */
+typedef int __locale_t;
+typedef __locale_t locale_t;
+#endif
+
 int isalnum(int c);
+/* TODO: isalnum_l */
 int isalpha(int c);
+/* TODO: isalpha_l */
+int isascii(int c);
+/* TODO: isascii_l */
 int isblank(int c);
+/* TODO: isblank_l */
 int iscntrl(int c);
+/* TODO: iscntrl_l */
 int isdigit(int c);
+/* TODO: isdigit_l */
 int isgraph(int c);
+/* TODO: isgraph_l */
 int islower(int c);
+/* TODO: islower_l */
 int isprint(int c);
+/* TODO: isprint_l */
 int ispunct(int c);
+/* TODO: ispunct_l */
 int isspace(int c);
+/* TODO: isspace_l */
 int isupper(int c);
+/* TODO: isupper_l */
 int isxdigit(int c);
+/* TODO: isxdigit_l */
 int tolower(int c);
+/* TODO: tolower_l */
 int toupper(int c);
+/* TODO: toupper_l */
 
 __END_DECLS
 
