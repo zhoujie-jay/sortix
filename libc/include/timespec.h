bcbc974a05680cf00fa89fe940ca731a60faed7d
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2014-05-09 16:08:50 +0200

    Fix usage of inline keyword in <timespec.h>.

--- libc/include/timespec.h
+++ libc/include/timespec.h
@@ -44,41 +44,41 @@ __END_DECLS
 
 __BEGIN_DECLS
 
-static inline bool timespec_eq(struct timespec a, struct timespec b)
+static __inline bool timespec_eq(struct timespec a, struct timespec b)
 {
 	return a.tv_sec == b.tv_sec && a.tv_nsec == b.tv_nsec;
 }
 
-static inline bool timespec_neq(struct timespec a, struct timespec b)
+static __inline bool timespec_neq(struct timespec a, struct timespec b)
 {
 	return a.tv_sec != b.tv_sec || a.tv_nsec != b.tv_nsec;
 }
 
-static inline bool timespec_lt(struct timespec a, struct timespec b)
+static __inline bool timespec_lt(struct timespec a, struct timespec b)
 {
 	return a.tv_sec < b.tv_sec ||
 	       (a.tv_sec == b.tv_sec && a.tv_nsec < b.tv_nsec);
 }
 
-static inline bool timespec_le(struct timespec a, struct timespec b)
+static __inline bool timespec_le(struct timespec a, struct timespec b)
 {
 	return a.tv_sec < b.tv_sec ||
 	       (a.tv_sec == b.tv_sec && a.tv_nsec <= b.tv_nsec);
 }
 
-static inline bool timespec_gt(struct timespec a, struct timespec b)
+static __inline bool timespec_gt(struct timespec a, struct timespec b)
 {
 	return a.tv_sec > b.tv_sec ||
 	       (a.tv_sec == b.tv_sec && a.tv_nsec > b.tv_nsec);
 }
 
-static inline bool timespec_ge(struct timespec a, struct timespec b)
+static __inline bool timespec_ge(struct timespec a, struct timespec b)
 {
 	return a.tv_sec > b.tv_sec ||
 	       (a.tv_sec == b.tv_sec && a.tv_nsec >= b.tv_nsec);
 }
 
-static inline struct timespec timespec_make(time_t sec, long nsec)
+static __inline struct timespec timespec_make(time_t sec, long nsec)
 {
 	struct timespec ret;
 	ret.tv_sec = sec;
@@ -86,14 +86,14 @@ static inline struct timespec timespec_make(time_t sec, long nsec)
 	return ret;
 }
 
-static inline struct timespec timespec_neg(struct timespec t)
+static __inline struct timespec timespec_neg(struct timespec t)
 {
 	if ( t.tv_nsec )
 		return timespec_make(-t.tv_sec - 1, 1000000000 - t.tv_nsec);
 	return timespec_make(-t.tv_sec, 0);
 }
 
-static inline struct timespec timespec_nul()
+static __inline struct timespec timespec_nul()
 {
 	return timespec_make(0, 0);
 }
