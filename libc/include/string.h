4ee15987fac1a48460d47ddb7db5a9c65c3ded8f
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2014-06-20 18:20:34 +0200

    Add timingsafe_memcmp(3).

--- libc/include/string.h
+++ libc/include/string.h
@@ -132,6 +132,7 @@ size_t strlcat(char* __restrict, const char* __restrict, size_t);
 size_t strlcpy(char* __restrict, const char* __restrict, size_t);
 /* TODO: strncasecmp_l */
 int strverscmp(const char*, const char*);
+int timingsafe_memcmp(const void*, const void*, size_t);
 #endif
 
 /* Functions that are Sortix extensions. */
