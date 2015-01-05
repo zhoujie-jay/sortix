6df61ecb3ae4239130db63bfa1849a9348b50055
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2015-01-05 20:33:43 +0100

    Add June 30 2015 leap second.

--- libc/time/gmtime_r.cpp
+++ libc/time/gmtime_r.cpp
@@ -90,6 +90,8 @@ static int8_t leap_seconds[][12] =
 	DECL_LEAP_SECOND(2011, 0, 0),
 	DECL_LEAP_SECOND(2012, 1, 0),
 	DECL_LEAP_SECOND(2013, 0, 0),
+	DECL_LEAP_SECOND(2014, 0, 0),
+	DECL_LEAP_SECOND(2015, 1, 0),
 };
 
 static time_t get_leap_second(int year, int month)
