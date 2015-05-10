4cb9ccfc4bbcf4e6ae197c809ec0a2a4b6d6d56b
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2015-05-10 23:50:27 +0200

    Rename libc/time/times.cpp to libc/sys/times/times.cpp.

--- libc/sys/times/times.cpp
+++ libc/sys/times/times.cpp
@@ -17,7 +17,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with the Sortix C Library. If not, see <http://www.gnu.org/licenses/>.
 
-    time/times.cpp
+    sys/times/times.cpp
     Get process execution time statistics.
 
 *******************************************************************************/
