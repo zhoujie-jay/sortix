aff8f4d89cefb2147f656f001c4fe942a36b7eb0
Jonas 'Sortie' Termansen <sortie@maxsi.org>
2016-02-03 01:00:21 +0100

    Fix files with incorrect copyright headers.

--- trianglix/vector.h
+++ trianglix/vector.h
@@ -1,3 +1,27 @@
+/*******************************************************************************
+
+    Copyright(C) Jonas 'Sortie' Termansen 2013.
+
+    This file is part of trianglix.
+
+    trianglix is free software: you can redistribute it and/or modify it under
+    the terms of the GNU Lesser General Public License as published by the Free
+    Software Foundation, either version 3 of the License, or (at your option)
+    any later version.
+
+    trianglix is distributed in the hope that it will be useful, but WITHOUT ANY
+    WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+    FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+    details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with trianglix. If not, see <http://www.gnu.org/licenses/>.
+
+    vector.h
+    Vector class.
+
+*******************************************************************************/
+
 #ifndef VECTOR_H
 #define VECTOR_H
 
